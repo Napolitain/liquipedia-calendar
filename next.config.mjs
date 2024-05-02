a49f2cc381b1a3d89edcc1d57c0c8f8dc148d83e
Maxime Boucher <mxboucher@gmail.com>
2024-05-01 21:17:07 -0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/nextjs-github-pages",
+    basePath: "/liquipedia-calendar",
 
     /**
      * Disable server-based image optimization. Next.js does not support
