412a7b055c92e36d741e6544ea016c5be60dd2e3
Maxime Boucher <mxboucher@gmail.com>
2024-05-01 19:37:49 -0700

    full static website prepared

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,173 @@
-import Image from "next/image";
+'use client';
+
+import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div>
+          {/* Top part is an input accepting URLs
+           * Entering resets the input
+           * Top part is an input accepting URLs
+           * Entering resets the input
+           */}
+          <div className={"part"}>
+            <label>Add a player (append link as below, and press enter or space)</label>
+            <input placeholder={"https://liquipedia.net/starcraft2/Serral"} autoFocus={true} onKeyDown={onKeyDown} type="text"/>
+          </div>
+          <div className={"part"}>
+            <label>Tracked players (click on element to remove)</label>
+              <span id={"tracked-players"}></span>
+          </div>
+          {/* Bottom part is the output link which, when clicked, is copied to the clipboard.*/}
+          <div className={"part"}>
+              <label>Output link (automatically copied to clipboard)</label>
+              <a href={"https://calendar.google.com/calendar/u/0/r/settings/addbyurl?pli=1"} id={"output-link"}>https://liquipedia-calendar.oa.r.appspot.com/?query=</a>
+          </div>
+          <div className={"part"}>
+              <label id={"final-redirect"} onClick={redirectToGoogleCalendar}>Append to calendar (click)</label>
+          </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
+}
+
+let trackedPlayers: Record<string, Set<string>> = {}; // Game -> Set of players
+
+/**
+ * On space or enter key press, add the URL to the list of tracked players and copy to clipboard.
+ */
+function onKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    /**
+     * Extracts the game and player from the input URL.
+     * @returns {game, player}
+     * @example https://liquipedia.net/starcraft2/Serral -> {game: "starcraft2", player: "Serral"}
+     */
+    function getQuery(): { game: string; player: string } {
+        // Add the URL to the list of tracked players
+        const input = event.target as HTMLInputElement;
+        const url = input.value;
+        // Validate the URL
+        if (!validateURL(url)) {
+            throw new Error("Invalid URL");
+        }
+        input.value = "";
+        // Get game and player value from the URL
+        // Split by /, [3] is the game, [4] is the player
+        const parts = url.split("/");
+        const game = parts[3];
+        const player = parts[4];
+        return {game, player};
+    }
+
+    function addToTrackedPlayers(game: string, player: string): void {
+        // Append to the list of tracked players
+        if (!trackedPlayers[game]) {
+            trackedPlayers[game] = new Set();
+        }
+        trackedPlayers[game].add(player);
+    }
+
+    function updateTrackedPlayersView() {
+        // Update the tracked players input
+        const trackedPlayersElement: HTMLElement = document.getElementById("tracked-players");
+        trackedPlayersElement.innerHTML = "";
+        for (const game in trackedPlayers) {
+            const g = trackedPlayers[game];
+            g.forEach(player => {
+                // Add a sticker game:player to the input
+                // If there are multiple players, separate them by commas
+                const span = document.createElement("span");
+                span.className = "tracked-player";
+                span.id = "tracked-player-" + game + "-" + player;
+                span.onclick = () => {
+                    // Remove the player from the list of tracked players
+                    trackedPlayers[game].delete(player);
+                    // Update the tracked players input
+                    document.getElementById(span.id).remove();
+                    // Update the output link
+                    updateOutputLink();
+                }
+                span.textContent = game + ":" + player;
+                trackedPlayersElement.appendChild(span);
+            })
+        }
+    }
+
+    if (event.key === "Enter" || event.key === " ") {
+        let game: string, player: string;
+        try {
+            ({game, player} = getQuery());
+        } catch (e) {
+            console.error(e);
+            return;
+        }
+        addToTrackedPlayers(game, player);
+        updateTrackedPlayersView();
+        // Update the output link
+        updateOutputLink();
+    }
+}
+
+/**
+ * Updates the output link with the current list of tracked players.
+ * The URL (CalDAV) is copied to the clipboard.
+ * The user is redirected to Google Calendar.
+ */
+function updateOutputLink() { //  g=starcraft2&p=maru,serral
+    const outputLink = document.getElementById("output-link") as HTMLAnchorElement;
+    let p = "https://liquipedia-calendar.oa.r.appspot.com/?query=";
+    for (const game in trackedPlayers) {
+        const g = trackedPlayers[game];
+        let n = g.size;
+        if (n === 0) {
+            continue;
+        }
+        p += stringToHex("g=" + game + "&p=");
+        let i = 0;
+        g.forEach(player => {
+            p += stringToHex(player);
+            if (i < n - 1) {
+                p += stringToHex(",");
+            }
+            i++;
+        })
+    }
+    outputLink.textContent = p;
+    // Copy to clipboard
+    navigator.clipboard.writeText(p);
+}
+
+/**
+ * Converts a string to a hexadecimal string.
+ * @param str The input string.
+ * @returns {string} The hexadecimal string.
+ * @example "Serral" -> "53757272616c"
+ */
+function stringToHex(str: string) {
+    let hex = '';
+    for (let i = 0; i < str.length; i++) {
+        hex += '' + str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
+
+/**
+ * Redirects the user to Google Calendar.
+ */
+function redirectToGoogleCalendar() {
+    // Redirect to Google Calendar
+    const outputLink = document.getElementById("output-link") as HTMLAnchorElement;
+    outputLink.click();
+}
+
+/**
+ * Validates the input URL.
+ * @param input The input URL.
+ * @returns {boolean} True if the URL is valid, false otherwise.
+ * @example https://liquipedia.net/starcraft2/Serral -> true
+ * @example https://liquipedia.net/starcraft2//Serral -> false
+ * @example https://liquipedia.net/starcraft2/Serral/ -> false
+ */
+function validateURL(input: string): boolean {
+    let regex = /https:\/\/liquipedia\.net\/[A-Za-z0-9]+\/[A-Za-z]+/i;
+    return regex.test(input);
 }
